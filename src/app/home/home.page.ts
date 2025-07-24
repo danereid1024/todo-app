70252324b4dae6ea0ed94fe385b922d69148bdfc
Dane Reid <forestlevelgames@gmail.com>
2025-07-24 08:57:41 -0400

    todo list ionic capacitor angular

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,48 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { DatePipe } from '@angular/common';
+import { NgFor } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonHeader, IonCheckbox, IonButton, IonList, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonCard, IonCardHeader, IonCardSubtitle, IonInput } from '@ionic/angular/standalone';
+import { NgClass } from '@angular/common';
+export interface TodoItem {
+  id: number;
+  task: string;
+  completed: boolean;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [FormsModule, NgClass, DatePipe, IonHeader, IonCheckbox, IonList, IonButton, IonToolbar, IonTitle, IonContent, IonLabel, IonItem, IonCard, IonCardHeader, IonCardSubtitle, IonInput, NgFor],
 })
 export class HomePage {
-  constructor() {}
+  
+  todoList : TodoItem [] = [];
+  newTask : string = '';
+  
+  addTask(): void {
+    if (this.newTask.trim() !== '') {
+      const newTodoItem : TodoItem = {
+        id : Date.now(),
+        task : this.newTask,
+        completed : false
+    }
+
+    this.todoList.push(newTodoItem);
+    this.newTask = ''; // Clear the input field after adding the task
+
+  }
 }
+
+toggleCompletion(index: number): void {
+    this.todoList[index].completed = !this.todoList[index].completed;
+  
+}
+
+deleteTask(id: number): void {
+    this.todoList = this.todoList.filter(item => item.id !== id);
+    console.log(this.todoList)
+  }
+}
+  
